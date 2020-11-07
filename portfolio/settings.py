77a6512ff6d2f4ba4a1ce9f6d283aa09bc4cca82
d-bryson-jones <1dbjones@gmail.com>
2020-11-07 15:01:07 -0800

    final version Nov72020

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': '$enoj678',
+        'PASSWORD': 'generic678',
         'HOST': 'localhost',
         'PORT': '5432',
     }
